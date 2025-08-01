9dd0b0ed2907a84b43c4425a46d2b85aa455e960
India R. <india.ratliff@gmail.com>
2025-08-01 14:51:59 -0500

    Redoing file structure to be more simple

--- models/Quiz.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const quizSchema = new mongoose.Schema({
-  userId:String,
-  type:String,
-  answers:Object,
-  summary:String,
-  createdAt:Date
-})
-module.exports = mongoose.model('Quiz',quizSchema)
