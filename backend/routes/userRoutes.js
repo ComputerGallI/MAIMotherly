31001b2a5abf287ef0dfc1ce36cbd39e91b4fe4e
India R. <india.ratliff@gmail.com>
2025-08-06 01:42:46 -0500

    userchagne

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,21 +2,21 @@ const express=require('express')
 const User=require('../models/User')
 const r=express.Router()
 
-//fake demo users
+// seed users
 r.get('/seed',async(req,res)=>{
- await User.insertMany([
-  {username:'IndieMovies',password:'12345!',quizResults:[],suggestions:[]},
-  {username:'Ryanator',password:'12345!',quizResults:[],suggestions:[]}
- ])
- res.send('seed ok')
+  await User.insertMany([
+    {username:'IndieMovies',password:'12345!',quizResults:[],suggestions:[]},
+    {username:'Ryanator',password:'12345!',quizResults:[],suggestions:[]}
+  ])
+  res.send('seeded users')
 })
 
-//login
+// login
 r.post('/login',async(req,res)=>{
- const{username,password}=req.body
- const u=await User.findOne({username})
- if(!u||u.password!==password)return res.status(401).json({error:'invalid'})
- res.json(u)
+  const{username,password}=req.body
+  const u=await User.findOne({username})
+  if(!u||u.password!==password)return res.status(401).json({error:'invalid'})
+  res.json(u)
 })
 
 module.exports=r
