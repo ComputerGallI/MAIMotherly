d82ca36de36dcc7fb08d9a485626feea7e611601
India R. <india.ratliff@gmail.com>
2025-08-06 01:43:37 -0500

    pythonupdate

--- fastapi_ai/main.py
+++ fastapi_ai/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
-import faiss, numpy as np
 from sentence_transformers import SentenceTransformer
+import faiss, numpy as np
 
 app = FastAPI()
 
+# retriever
 retriever_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# generator
 generator = pipeline("text2text-generation", model="facebook/bart-large")
 
-texts = ["feeling anxious","i am happy today","work stress is high"]
+texts = ["Feeling happy today", "Overwhelmed with stress", "Need some rest"]
 index = faiss.IndexFlatL2(384)
 embeddings = retriever_model.encode(texts)
 index.add(np.array(embeddings))
@@ -19,11 +22,10 @@ class ChatRequest(BaseModel):
     quiz_summary:str
 
 @app.post("/generate")
-def generate_response(req: ChatRequest):
+def generate(req: ChatRequest):
     vec = retriever_model.encode([req.user_input])
     D,I = index.search(np.array(vec),3)
     docs = [texts[i] for i in I[0]]
-    context = " ".join(docs)
-    prompt = f"Context: {context}. Traits: {req.quiz_summary}. Question: {req.user_input}"
+    prompt = f"Context: {docs}. Traits: {req.quiz_summary}. User: {req.user_input}"
     response = generator(prompt,max_length=120,do_sample=False)
     return {"response":response[0]['generated_text'],"retrieved_docs":docs}
